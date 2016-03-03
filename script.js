44a023ab71183bd0094da717f871897dc6bbf5fb
Lukas Hagman <Lukas.Hagman@Multisoft.se>
2016-03-03 15:08:41 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,70 @@
+// ==UserScript==
+// @name         rfc navigatiser
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  make rfs less awful
+// @author       github/koneke
+// @match        *.ietf.org/rfc/rfc*
+// @grant        none
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+function CreateAnchors()
+{
+    var content = GetContent();
+    var rexp = /\[Page (\d+)\]/g;
+    while (true)
+    {
+        var match = rexp.exec(content);
+        if (!match)
+        {
+            break;
+        }
+        var matchEnd = match.index + match.toString().length;
+        
+        content = content.substr(0, match.index) +
+            '<a name="page' + match[1] + '" href="#">(Page ' + match[1] + ')' + '</a>' +
+            content.substr(matchEnd - 1, content.length - matchEnd);
+    }
+    SetContent(content);
+}
+
+function GetContent(to)
+{
+    return document.getElementsByTagName("pre")[0].innerHTML;
+}
+
+function SetContent(to)
+{
+    document.getElementsByTagName("pre")[0].innerHTML = to;
+}
+
+function HackToC()
+{
+    var content = '<span>' + GetContent() + '</span>';
+    SetContent(content);
+    var rexp = /[0-9.]+\s[^.]+\s\.+\s+(\d+)\s*[\r|\n]/g;
+    while (true)
+    {
+        var match = rexp.exec(content);
+        if (!match)
+        {
+            console.log("nah");
+            break;
+        }
+        var pageNum = match[1];
+        var subexp = /(\d+)\s*[\r|\n]/;
+        var submatch = subexp.exec(match[0]);
+        var matchEnd = match.index + submatch.index + submatch[0].length;
+        var replacement = '<a href="#page' + pageNum + '" href="#">p' + submatch[0] + '</a>';
+        
+        content = content.substr(0, match.index + submatch.index) +
+            replacement +
+            content.substr(matchEnd, content.length - matchEnd);
+    }
+    SetContent(content);
+}
+
+CreateAnchors();
+HackToC();
